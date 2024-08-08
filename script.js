ad51a42088a2d0a712562e4c27f40e4970dc469d
Anik2812 <anikshaikh28125@gmail.com>
2024-08-08 20:03:02 +0530

    ui update

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 // Toggle add to cart container on wishlist icon click
 const wishlistIcon = document.querySelector('.wishlist-icon');
 const addToCartContainer = document.querySelector('.add-to-cart-container');
+let isChainPulled = false;
 
 wishlistIcon.addEventListener('click', () => {
-  addToCartContainer.classList.toggle('show');
+  if (!isChainPulled) {
+    addToCartContainer.classList.add('show');
+    isChainPulled = true;
+    animateChain();
+  } else {
+    addToCartContainer.classList.remove('show');
+    isChainPulled = false;
+    stopChainAnimation();
+  }
 });
 
 // Add to cart functionality
@@ -18,4 +27,26 @@ addToCartBtn.addEventListener('click', () => {
   cartItems += quantity;
   cartCount.textContent = cartItems;
   addToCartContainer.classList.remove('show');
+  isChainPulled = false;
+  stopChainAnimation();
 });
+
+// Chain animation
+let chainAnimationInterval;
+
+function animateChain() {
+  chainAnimationInterval = setInterval(() => {
+    const chainLinks = document.querySelectorAll('.chain-link');
+    chainLinks.forEach((link, index) => {
+      link.style.transform = `rotate(${index * 45}deg)`;
+    });
+  }, 500);
+}
+
+function stopChainAnimation() {
+  clearInterval(chainAnimationInterval);
+  const chainLinks = document.querySelectorAll('.chain-link');
+  chainLinks.forEach((link) => {
+    link.style.transform = 'rotate(0deg)';
+  });
+}
